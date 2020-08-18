943084c605a7ac5f96cf7de17ef5f517fad7b2b9
Hieu Nguyen <minhhieun4@gmail.com>
2020-08-18 10:31:52 +0700

    fix

---
+++ model/cnn/conv2d.py
@@ -0,0 +1,167 @@
+from keras.layers import Conv2D, BatchNormalization, MaxPooling2D, UpSampling2D
+from keras.models import Sequential
+import numpy as np
+from model import common_util
+import model.utils.conv2d as utils_conv2d
+import os
+import yaml
+from pandas import read_csv
+from keras.utils import plot_model
+from keras import backend as K
+from keras.losses import mse
+
+
+class Conv2DSupervisor():
+    def __init__(self, **kwargs):
+        self.config_model = common_util.get_config_model(**kwargs)
+
+        # load_data
+        self.data = utils_conv2d.load_dataset(**kwargs)
+        self.input_train = self.data['input_train']
+        self.input_valid = self.data['input_valid']
+        self.input_test = self.data['input_test']
+        self.target_train = self.data['target_train']
+        self.target_valid = self.data['target_valid']
+        self.target_test = self.data['target_test']
+
+        # other configs
+        self.log_dir = self.config_model['log_dir']
+        self.optimizer = self.config_model['optimizer']
+        self.loss = self.config_model['loss']
+        self.activation = self.config_model['activation']
+        self.batch_size = self.config_model['batch_size']
+        self.epochs = self.config_model['epochs']
+        self.callbacks = self.config_model['callbacks']
+        self.seq_len = self.config_model['seq_len']
+        self.horizon = self.config_model['horizon']
+
+        self.model = self.build_model_prediction()
+
+    def build_model_prediction(self):
+        model = Sequential()
+
+        # Input
+        model.add(
+            Conv2D(filters=64,
+                   kernel_size=(3, 3),
+                   padding='same',
+                   activation=self.activation,
+                   name='input_layer_conv2d',
+                   input_shape=(160, 120, 1)))
+        model.add(BatchNormalization())
+
+        # Max Pooling - Go deeper
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(
+            Conv2D(64, (3, 3),
+                   activation='relu',
+                   padding='same',
+                   name='hidden_conv2d_1'))
+        model.add(BatchNormalization())
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(
+            Conv2D(64, (3, 3),
+                   activation='relu',
+                   padding='same',
+                   name='hidden_conv2d_2'))
+        model.add(BatchNormalization())
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(
+            Conv2D(64, (3, 3),
+                   activation='relu',
+                   padding='same',
+                   name='hidden_conv2d_3'))
+        model.add(BatchNormalization())
+
+        # Up Sampling
+        model.add(UpSampling2D(size=(2, 2)))
+        model.add(
+            Conv2D(64, (3, 3),
+                   activation='relu',
+                   padding='same',
+                   name='hidden_conv2d_4'))
+        model.add(BatchNormalization())
+        model.add(UpSampling2D(size=(2, 2)))
+        model.add(
+            Conv2D(64, (3, 3),
+                   activation='relu',
+                   padding='same',
+                   name='hidden_conv2d_5'))
+        model.add(BatchNormalization())
+        model.add(UpSampling2D(size=(2, 2)))
+        model.add(
+            Conv2D(64, (3, 3),
+                   activation='relu',
+                   padding='same',
+                   name='hidden_conv2d_6'))
+        model.add(BatchNormalization())
+
+        model.add(
+            Conv2D(filters=1,
+                   kernel_size=(3, 3),
+                   padding='same',
+                   name='output_layer_conv2d',
+                   activation=self.activation))
+        print(model.summary())
+
+        # plot model
+        from keras.utils import plot_model
+        plot_model(model=model,
+                   to_file=self.log_dir + '/conv2d_model.png',
+                   show_shapes=True)
+        return model
+
+    def train(self):
+        self.model.compile(optimizer=self.optimizer,
+                           loss=self.loss,
+                           metrics=['mse', 'mae'])
+
+        training_history = self.model.fit(self.input_train,
+                                          self.target_train,
+                                          batch_size=self.batch_size,
+                                          epochs=self.epochs,
+                                          callbacks=self.callbacks,
+                                          validation_data=(self.input_valid,
+                                                           self.target_valid),
+                                          shuffle=True,
+                                          verbose=1)
+
+        if training_history is not None:
+            common_util._plot_training_history(training_history,
+                                               self.config_model)
+            common_util._save_model_history(training_history,
+                                            self.config_model)
+            config = dict(self.config_model['kwargs'])
+
+            # create config file in log again
+            config_filename = 'config.yaml'
+            config['train']['log_dir'] = self.log_dir
+            with open(os.path.join(self.log_dir, config_filename), 'w') as f:
+                yaml.dump(config, f, default_flow_style=False)
+
+    def test_prediction(self):
+        print("Load model from: {}".format(self.log_dir))
+        self.model.load_weights(self.log_dir + 'best_model.hdf5')
+        self.model.compile(optimizer=self.optimizer, loss=self.loss)
+
+        groundtruth = []
+        preds = []
+
+        groundtruth = np.array(groundtruth)
+        preds = np.array(preds)
+        np.savetxt(self.log_dir + 'list_metrics.csv', list_metrics, delimiter=",")
+        np.savetxt(self.log_dir + 'groundtruth.csv', groundtruth, delimiter=",")
+        np.savetxt(self.log_dir + 'preds.csv', preds, delimiter=",")
+
+    def plot_result(self):
+        from matplotlib import pyplot as plt
+        preds = read_csv(self.log_dir + 'preds.csv')
+        gt = read_csv(self.log_dir + 'groundtruth.csv')
+        preds = preds.to_numpy()
+        gt = gt.to_numpy()
+        for i in range(0,3):
+            plt.plot(preds[i,:], label='preds')
+            plt.plot(gt[i,:], label='gt')
+            plt.legend()
+            plt.savefig(self.log_dir + 'result_predict_{}.png'.format(i))
+            plt.close()
---
+++ model/cnn/utils.py
@@ -0,0 +1,65 @@
+import numpy as np
+from model import common_util
+from sklearn.preprocessing import MinMaxScaler
+from keras import backend as K
+
+def create_data_prediction(**kwargs):
+
+    data_npz = kwargs['data'].get('dataset')
+    seq_len = kwargs['model'].get('seq_len')
+    horizon = kwargs['model'].get('horizon')
+
+    time = np.load(data_npz)['time']
+    # horizon is in seq_len. the last
+    T = len(time)
+
+    map_lon = np.load(data_npz)['map_lon']
+    map_lat = np.load(data_npz)['map_lat']
+    map_precip = np.load(data_npz)['map_precip']
+
+    gauge_lon = np.load(data_npz)['gauge_lon']
+    gauge_lat = np.load(data_npz)['gauge_lat']
+    gauge_precip = np.load(data_npz)['gauge_precip']
+
+    raw_precip_gsmap = np.load(data_npz)['raw_precip_gsmap']
+
+    # input is gsmap
+    input_model = np.zeros(shape=(T, 160, 120, 1))
+    # output is gauge
+    output_model = np.zeros(shape=(T, 160, 120, 1))
+
+    for i in range(len(gauge_lat)):
+        lat = map_lat[i]
+        lon = map_lon[i]
+        temp_lat = int(round((23.95 - lat) / 0.1))
+        temp_lon = int(round((lon - 100.05) / 0.1))
+        for index_lat in range(temp_lat-4, temp_lat+5):
+            for index_lon in range(temp_lon-4, temp_lon+5):
+                input_model[:, index_lat, index_lon, 0] = map_precip[:, i]
+                output_model[:, index_lat, index_lon, 0] = map_precip[:, i]
+            
+        input_model[:, temp_lat, temp_lon, 0] = map_precip[:, i]
+        output_model[:, temp_lat, temp_lon, 0] = gauge_precip[:, i]
+    return input_model, output_model
+
+
+def load_dataset(**kwargs):
+    # get preprocessed input and target
+    input_conv2d_gsmap, target_conv2d_gsmap = create_data_prediction(**kwargs)
+
+    # get test_size, valid_size from config
+    test_size = kwargs['data'].get('test_size')
+    valid_size = kwargs['data'].get('valid_size')
+
+    # split data to train_set, valid_set, test_size
+    input_train, input_valid, input_test = common_util.prepare_train_valid_test(
+        input_conv2d_gsmap, test_size=test_size, valid_size=valid_size)
+    target_train, target_valid, target_test = common_util.prepare_train_valid_test(
+        target_conv2d_gsmap, test_size=test_size, valid_size=valid_size)
+    data = {}
+    for cat in ["train", "valid", "test"]:
+        x, y = locals()["input_" + cat], locals()["target_" + cat]
+        data["input_" + cat] = x
+        data["target_" + cat] = y
+
+    return data
